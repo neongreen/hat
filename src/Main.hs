324101faee5b04ae1d394fc78d44095ef5af94a6
Artyom <yom@artyom.me>
2016-05-26 15:59:39 +0300

    Something something

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,95 @@
+{-# LANGUAGE
+OverloadedStrings,
+TypeFamilies,
+NoImplicitPrelude
+  #-}
+
+
 module Main (main) where
 
+
+-- General
+import BasePrelude
+-- Lenses
+import Lens.Micro.Platform
+-- Monads
+import Control.Monad.IO.Class
+-- Web
+import Web.Spock hiding (head, get, text)
+import qualified Web.Spock as Spock
+import Web.Spock.Lucid
+import Lucid hiding (for_)
+-- acid-state
+import Data.Acid as Acid
+-- IO
+import System.IO
+
+-- Local
+import Types
+
+
+data ServerState = ServerState {
+  _db :: DB }
+
+type DB = AcidState GlobalState
+
+-- | Update something in the database.
+dbUpdate :: (MonadIO m, HasSpock m, SpockState m ~ ServerState,
+             EventState event ~ GlobalState, UpdateEvent event)
+         => event -> m (EventResult event)
+dbUpdate x = do
+  db <- _db <$> Spock.getState
+  liftIO $ do
+    Acid.update db SetDirty
+    Acid.update db x
+
+-- | Read something from the database.
+dbQuery :: (MonadIO m, HasSpock m, SpockState m ~ ServerState,
+            EventState event ~ GlobalState, QueryEvent event)
+        => event -> m (EventResult event)
+dbQuery x = do
+  db <- _db <$> Spock.getState
+  liftIO $ Acid.query db x
+
+-- | Like 'createCheckpoint', but doesn't create a checkpoint if there were
+-- no changes made.
+createCheckpoint' :: MonadIO m => DB -> m ()
+createCheckpoint' db = liftIO $ do
+  wasDirty <- Acid.update db UnsetDirty
+  when wasDirty $ do
+    createArchive db
+    createCheckpoint db
+
 main :: IO ()
-main = return ()
+main = do
+  hSetBuffering stdout NoBuffering
+  let emptyState = GlobalState {
+        _users = [],
+        _games = [],
+        _dirty = True }
+  let prepare = openLocalStateFrom "state/" sampleState
+      finalise db = do
+        createCheckpoint' db
+        closeAcidState db
+  bracket prepare finalise $ \db -> do
+    let serverState = ServerState {
+          _db = db }
+    let spockConfig = defaultSpockCfg () PCNoDatabase serverState
+    runSpock 7070 $ spock spockConfig $ do
+      Spock.get root $ do
+        s <- dbQuery GetGlobalState
+        lucid $ do
+          h1_ "Hat"
+          h2_ "Available games"
+          ul_ $ for_ (s ^. games) $ \game ->
+            li_ $ mkLink (toHtml (game^.title))
+                         ("game/" <> uidToText (game^.uid))
+      Spock.get ("game" <//> var) $ \gameId -> do
+        game <- dbQuery (GetGame gameId)
+        lucid $ do
+          h1_ (toHtml (game^.title))
+          p_ $ do
+            toHtml $ "game begins: " ++ show (game^.begins)
+
+mkLink :: Monad m => HtmlT m a -> Url -> HtmlT m a
+mkLink x src = a_ [href_ src] x
---
+++ src/Types.hs
@@ -0,0 +1,188 @@
+{-# LANGUAGE
+TemplateHaskell,
+TypeFamilies,
+TupleSections,
+MultiParamTypeClasses,
+FunctionalDependencies,
+FlexibleInstances,
+OverloadedStrings,
+GeneralizedNewtypeDeriving,
+FlexibleContexts,
+RankNTypes,
+NoImplicitPrelude
+  #-}
+
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+
+module Types
+(
+  -- * Common types
+  Uid(..),
+  Url,
+
+  -- * Specific types
+  User(..),
+    nick,
+    name,
+    pass,
+    admin,
+  Game(..),
+    title,
+    begins,
+    ended,
+    players,
+  GlobalState(..),
+    games,
+    users,
+    dirty,
+
+  -- * Common lenses
+  uid,
+
+  -- * Stuff
+  sampleState,
+
+  -- * Methods
+  GetGlobalState(..),
+  GetUser(..),
+  GetUserByNick(..),
+  GetGame(..),
+  SetDirty(..),
+  UnsetDirty(..),
+)
+where
+
+
+-- General
+import BasePrelude
+-- Monads
+import Control.Monad.State
+-- Lenses
+import Lens.Micro.Platform
+-- Text
+import qualified Data.Text as T
+import Data.Text (Text)
+-- Time
+import Data.Time
+-- Web
+import Web.PathPieces
+-- acid-state
+import Data.Acid as Acid
+import Data.SafeCopy
+-- Passwords
+import Crypto.Scrypt
+
+
+deriveSafeCopySimple 0 'base ''EncryptedPass
+
+newtype Uid a = Uid {uidToText :: Text}
+  deriving (Eq, Ord, Show, PathPiece)
+
+deriveSafeCopySimple 0 'base ''Uid
+
+type Url = Text
+
+data User = User {
+  _userUid :: Uid User,
+  _userNick :: Text,
+  _userName :: Text,
+  _userPass :: EncryptedPass,
+  _userAdmin :: Bool }
+  deriving (Show)
+
+deriveSafeCopySimple 0 'base ''User
+makeFields ''User
+
+data Game = Game {
+  _gameUid :: Uid Game,
+  _gameTitle :: Text,
+  _gameBegins :: UTCTime,
+  _gameEnded :: Bool,
+  _gamePlayers :: [Uid User] }
+  deriving (Show)
+
+deriveSafeCopySimple 0 'base ''Game
+makeFields ''Game
+
+data GlobalState = GlobalState {
+  _users :: [User],
+  _games :: [Game],
+  _dirty :: Bool }
+  deriving (Show)
+
+deriveSafeCopySimple 0 'base ''GlobalState
+makeLenses ''GlobalState
+
+hasUid :: HasUid a (Uid u) => Uid u -> a -> Bool
+hasUid u x = x^.uid == u
+
+userById :: Uid User -> Lens' GlobalState User
+userById uid' = singular $
+  users.each . filtered (hasUid uid') `failing`
+  error ("userById: couldn't find user with uid " ++
+         T.unpack (uidToText uid'))
+
+userByNick :: Text -> Lens' GlobalState User
+userByNick nick' = singular $
+  users.each . filtered ((== nick') . view nick) `failing`
+  error ("userById: couldn't find user with nick " ++
+         T.unpack nick')
+
+gameById :: Uid Game -> Lens' GlobalState Game
+gameById uid' = singular $
+  games.each . filtered (hasUid uid') `failing`
+  error ("gameById: couldn't find game with uid " ++
+         T.unpack (uidToText uid'))
+
+sampleState :: GlobalState
+sampleState = GlobalState {
+  _users = [
+      User {
+          _userUid = Uid "user100",
+          _userNick = "cooler",
+          _userName = "Mr. Cooler",
+          _userPass = encryptPass' salt (Pass "password"),
+          _userAdmin = True } ],
+  _games = [
+      Game {
+          _gameUid = Uid "game100",
+          _gameTitle = "Awesome game",
+          _gameBegins = read "2016-06-03 12:20:06 UTC",
+          _gameEnded = False,
+          _gamePlayers = [] } ],
+  _dirty = True }
+  where
+    salt = Salt "*xxxPxxxxxx)xHL#nx~z2xPxxxxvxxx#"
+
+-- | A useful lens operator that modifies something and returns the old value.
+(<<.=) :: MonadState s m => LensLike ((,) a) s s a b -> b -> m a
+(<<.=) l b = state (l (,b))
+{-# INLINE (<<.=) #-}
+infix 4 <<.=
+
+getGlobalState :: Acid.Query GlobalState GlobalState
+getGlobalState = view id
+
+getUser :: Uid User -> Acid.Query GlobalState User
+getUser uid' = view (userById uid')
+
+getUserByNick :: Text -> Acid.Query GlobalState User
+getUserByNick nick' = view (userByNick nick')
+
+getGame :: Uid Game -> Acid.Query GlobalState Game
+getGame uid' = view (gameById uid')
+
+setDirty :: Acid.Update GlobalState ()
+setDirty = dirty .= True
+
+unsetDirty :: Acid.Update GlobalState Bool
+unsetDirty = dirty <<.= False
+
+makeAcidic ''GlobalState [
+  'getGlobalState,
+  'getUser,
+  'getUserByNick,
+  'getGame,
+  'setDirty, 'unsetDirty
+  ]
